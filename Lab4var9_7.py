34f797e5f6e116feec0dbbe5291210e8af2b582c
Виктория <k.vika.v16@gmail.com>
2024-11-26 14:02:48 +0300

    Add files via upload
    Лабораторная работа №4 вариант 9 (Lab4var9.py) + доп задание про рюкзак с 7-ю ячейками (Lab4var9_7.py)

---
+++ Lab4var9_7.py
@@ -0,0 +1,62 @@
+# Для варианта c 7-ю ячейками :
+backpack = []
+inventory = [['empty' for _ in range(7)]]  # инвентарь
+size_backpack = 1*7  # размер рюкзака (1*7)
+weight = 0  # общий вес в рюкзаке
+points = 26  # начальное количество очков (можно менять!!!)
+total_points = points  # итоговое количество очков выживания
+
+# Предметы и их ценность
+items = [
+    ('r', 3, 25),  # Винтовка (rifle)
+    ('p', 2, 15),  # Пистолет (pistol)
+    ('a', 2, 15),  # Боекомплект (ammo)
+    ('m', 2, 20),  # Аптечка (medkit)
+    ('i', 1, 5),   # Ингалятор (inhaler)
+    ('k', 1, 15),  # Нож (knife)
+    ('x', 3, 20),  # Топор (axe)
+    ('t', 1, 25),  # Оберег (talisman)
+    ('f', 1, 15),  # Фляжка (flask)
+    ('d', 1, 10),  # Антидот (antidot)
+    ('s', 2, 20),  # Еда (supplies)
+    ('c', 2, 20)   # Арбалет (crossbow)
+]
+
+# Сортируем предметы
+sorted_items = sorted(items, key=lambda x: x[2] / x[1], reverse=True)
+
+# поскольку в данном варианте он ничем не болеет, то проверку на антидот и тд не делаем
+
+# Добавляем предметы в рюкзак
+for item in sorted_items:
+    if weight + item[1] <= size_backpack:
+        backpack.append(item)
+        weight += item[1]
+        total_points += item[2]
+
+# Уменьшаем очки за не добавленные предметы
+for item in sorted_items:
+    if item not in backpack and weight + item[1] > size_backpack:
+        total_points -= item[2]
+
+# Заполняем инвентарь
+number_slot = 0 # кол-во предметов в ИТОГОВОМ инвентаре inventory
+for j in range(len(backpack)):
+    nums = backpack[j][1] # кол-во ячеек, которое занимает предмет
+    for l in range(nums):
+        if number_slot < size_backpack:  # Проверяем, что не превышаем размер инвентаря
+            x = number_slot // 7
+            y = number_slot % 7
+            inventory[x][y] = backpack[j][0]
+            number_slot += 1
+
+# Результаты для варианта c 7-ю ячейками:
+if total_points > 0:
+    print('Весь инвентарь (1*7):')
+    for i in inventory:
+        print(i)
+    print(f'Очки выживания: {total_points}')
+else:
+    print('Количество очков выживания отрицательное или равно нулю')
+
+# ИТОГ: если кол-во очков выживания при рюкзаке в 7 ячеек больше 25, то общ. кол-во очков положительное
